366d058b7fcd6b1527733914971017246bc99180
Christian Röpke <croepke@posteo.de>
2019-06-15 12:05:40 +0200

    feat: Hyperparameter Optimization

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -9,15 +9,15 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 512        # minibatch size
 GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
+TAU = 2e-1              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor
 LR_CRITIC = 3e-4        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
-TIMESTEP_INTERVAL=20    # update every X timesteps
-N_UPDATES=10            # number of updates per agent update
+TIMESTEP_INTERVAL=5    # update every X timesteps
+N_UPDATES=10           # number of updates per agent update
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
